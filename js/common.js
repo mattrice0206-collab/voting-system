014b9e9ec6d2ef2de8a2840abc0644b1452c5232
mattrice0206-collab <mattrice0206@gmail.com>
2026-08-05 19:02:28 -0500

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,1 +1,27 @@
+import {
+    db,
+    doc,
+    collection
+} from "./firebase.js";
 
+// Get or create a unique device ID
+export function getDeviceId() {
+
+    let id = localStorage.getItem("deviceId");
+
+    if (!id) {
+
+        id = crypto.randomUUID();
+
+        localStorage.setItem("deviceId", id);
+
+    }
+
+    return id;
+
+}
+
+// Firestore references
+export const roomRef = doc(db, "rooms", "main");
+
+export const votersRef = collection(roomRef, "voters");
